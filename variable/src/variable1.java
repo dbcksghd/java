a62027385b0c49f179b768da402744781f830dfa
데탑 유찬홍 <chanhongy6@naver.com>
2022-07-29 20:17:55 +0900

    변수 선언

---
+++ variable/src/variable1.java
@@ -0,0 +1,8 @@
+
+public class variable1 {
+    public static void main(String[] args) {
+        int level;
+        level = 10;
+        System.out.println(level);
+    }
+}
---
+++ variable/src/variable2.java
@@ -0,0 +1,7 @@
+
+public class variable2 {
+    public static void main(String[] args) {
+        int level = 10;
+        System.out.println(level);
+    }
+}
