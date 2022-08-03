3495e52b1aafc68a964b8d3bfe2c3530de2093af
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-04 01:19:47 +0900

    반복문 예시

---
+++ loopexample/src/BasicLoop.java
@@ -0,0 +1,17 @@
+public class BasicLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        num+=2;
+        num+=3;
+        num+=4;
+        num+=5;
+        num+=6;
+        num+=7;
+        num+=8;
+        num+=9;
+        num+=10;
+        System.out.println("1부터 10까지의 합은 " + num + "입니다.");
+    }
+}
+
+//딱봐도 너무 비효율적인것 같다. 이렇게 반복되는 일을 처리하기 위해 사용하는것이 반복문이다.
