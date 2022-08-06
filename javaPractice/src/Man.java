882fce48dc561e72d1e6e7c36e44f5305e36882a
데탑 유찬홍 <chanhongy6@naver.com>
2022-08-06 14:57:55 +0900

    자바 연습장

---
+++ javaPractice/src/Man.java
@@ -0,0 +1,27 @@
+public class Man {
+
+    int age;
+    String name;
+    boolean isMarried;
+    int children;
+
+
+    public static void main(String[] args) {
+        Man nameMan = new Man();
+        nameMan.name = "James";
+
+        Man ageMan = new Man();
+        ageMan.age = 40;
+
+        Man isMarriedMan = new Man();
+        isMarriedMan.isMarried = true;
+
+        Man childrenMan = new Man();
+        childrenMan.children = 3;
+
+        System.out.println(nameMan.name);
+        System.out.println(ageMan.age);
+        System.out.println(isMarriedMan.isMarried);
+        System.out.println(childrenMan.children);
+    }
+}
