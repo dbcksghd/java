6af341293c616c2ca752f921ad496764377a6407
맥북 유찬홍 <chanhongy6@naver.com>
2023-03-16 09:31:06 +0900

    입력받은 숫자 출력하기

--- 자바 수업/scanner/scanner/src/Test.java
+++ 자바 수업/scanner/scanner/src/Test.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
 public class Test {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        System.out.println(input);
+    }
 }
