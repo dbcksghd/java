44a6288aba03e5e3ffd628fff69728c306ae11c4
yoochanhong <chanhongy6@naver.com>
2023-04-19 17:59:16 +0900

    메인 클래스

--- 자바 수업/objectModeling/object_modeling/src/Main.java
+++ 자바 수업/objectModeling/object_modeling/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person person = new Person("유찬홍");
+        Person person1 = new Person("엄준식");
+        ArrayList<String> personList = new ArrayList<>();
+        personList.add(person1.name);
+        personList.add(person.name);
+
+        Studyroom studyroom = new Studyroom("4층 자습실", personList);
+        studyroom.getStudingPeopleName();
+        studyroom.study();
+
+        SpecialStudyRoom specialStudyRoom = new SpecialStudyRoom("가온실", personList);
+        specialStudyRoom.turnOnTVByPerson(person.name);
+
+        ArrayList<String> roomLiveList = new ArrayList<>(List.of("유찬홍", "박준하", "이재동"));
+        Room room = new Room(roomLiveList, 408);
+        room.getRoomNumber();
+        room.getRoomLivingPersonList();
+        room.sleep();
     }
 }
