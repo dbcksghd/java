e9b22609836f8a501d553a99bf6f56d2a9a260a7
yoochanhong <chanhongy6@naver.com>
2023-04-19 22:29:47 +0900

    완성

--- 자바 수업/objectModeling/object_modeling/src/Main.java
+++ 자바 수업/objectModeling/object_modeling/src/Main.java
@@ -4,23 +4,25 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Person person = new Person("유찬홍");
-        Person person1 = new Person("엄준식");
-        ArrayList<String> personList = new ArrayList<>();
-        personList.add(person1.name);
-        personList.add(person.name);
+        Student student = new Student("유찬홍", 14);
+        Student student1 = new Student("엄준식", 10);
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(student);
+        studentList.add(student1);
 
-        Studyroom studyroom = new Studyroom("4층 자습실", personList);
-        studyroom.getStudingPeopleName();
+        Studyroom studyroom = new Studyroom("4층 자습실", studentList);
         studyroom.study();
+        studyroom.getRoomUsePersonList();
 
-        SpecialStudyRoom specialStudyRoom = new SpecialStudyRoom("가온실", personList);
-        specialStudyRoom.turnOnTVByPerson(person.name);
-
-        ArrayList<String> roomLiveList = new ArrayList<>(List.of("유찬홍", "박준하", "이재동"));
-        Room room = new Room(roomLiveList, 408);
-        room.getRoomNumber();
-        room.getRoomLivingPersonList();
+        SpecialStudyRoom specialStudyRoom = new SpecialStudyRoom("가온실", studentList);
+        specialStudyRoom.turnOnTVByStudent();
+        Student student2 = new Student("박준하", 8);
+        Student student3 = new Student("이재동", 15);
+        ArrayList<Student> roomLiveList = new ArrayList<>(List.of(student, student2, student3));
+        Room room = new Room("408호", roomLiveList);
+        room.getRoomName();
+        room.getRoomUsePersonList();
         room.sleep();
+        room.sleep(12, 10);
     }
 }
