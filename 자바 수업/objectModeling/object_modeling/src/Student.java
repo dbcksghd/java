60dcb8cd990279772695c6cf81919a8cd43b8ce6
yoochanhong <chanhongy6@naver.com>
2023-04-19 23:24:32 +0900

    사용 관계 추가

--- 자바 수업/objectModeling/object_modeling/src/Student.java
+++ 자바 수업/objectModeling/object_modeling/src/Student.java
@@ -1,11 +1,16 @@
 public class Student extends Person {
     final private int number;
+
     Student(String name, int number) {
         super(name);
         this.number = number;
     }
 
-    public int getNumber(){
+    public int getNumber() {
         return number;
+    }
+
+    public void turnOnTV(Television television) {
+        System.out.println(getName() + television.turnOnTV());
     }
 }
